f20f8e2b0f2ef59bc9330e905844e6e5694d67e4
MuPrince <137839462@qq.com>
2025-03-20 16:23:18 +0800

    完成简单通证合约

--- contract/fund/FundToken.sol
+++ contract/fund/FundToken.sol
@@ -1,18 +1,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract FundToken is ERC20 {
+contract FundToken {
     // 1. 通证名称
     // 2. 通证简称
     // 3. 通证发行数量
     // 4. owner地址
     // 5. balance address => uint256
+    string public name;
+    string public symbol;
+    uint256 public totalSupply;
+    address public owner;
+    mapping (address => uint256) private balances;
+    
+    constructor(string memory _name, string memory _symbol) {
+        name = _name;
+        symbol = _symbol;
+        owner = msg.sender;  // 构造函数中初始化owner
+    }
 
     // mint: 获取通证
-    // transfer： transfer通证
-    // balanceOf：查看某一个地址的通证数量
+    function mint(uint256 amountToMint) public {
+        balances[msg.sender] += amountToMint;
+        totalSupply += amountToMint;
+    }
 
-    constructor() ERC20("FundToken", "FT") {}
+    // transfer： transfer通证
+    function transferToken(address payee, uint256 amount) public {
+        // 1. balances[msg.sender] => 表示当前地址拥有多少通证
+        require(balances[msg.sender] >= amount, "balance not enough"); // 如果余额不足
+        
+        balances[msg.sender] -= amount;
+        balances[payee] += amount;
+    }
+
+    // balanceOf：查看某一个地址的通证数量
+    function balanceOf(address _account) public view returns(uint256){
+        return balances[_account];
+    }
+    
+
+
 }
